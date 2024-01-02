db016298a5d0c5a526a730d6f6a10f4ad1ada0e8
unknown-bytes <unknown.develops@gmail.com>
2024-01-02 17:50:01 -0500

    ...

---
+++ src/components/Routes.js
@@ -0,0 +1,9 @@
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+
+export const Routes = () => {
+    return (
+        <BrowserRouter>
+            
+        </BrowserRouter>
+    )
+}
